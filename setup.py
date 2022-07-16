5c5f872eac1be7cce9ffd85a6baf694397a75b32
trnciii <doityan223@gmail.com>
2022-07-16 23:15:21 +0900

    fix setup

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-	name='gl2f-cli',
+	name='gl2f',
 	version='0.0.1',
 	url='https://github.com/trnciii/gl2f-cli',
 	license='MIT',
-	packages=find_packages(),
+
+	packages=['gl2f'],
+	install_requires=[
+		'requests',
+	],
+
 	entry_points={
 		'console_scripts': [
 			'gl2blogs = gl2f.blogs:ls'
