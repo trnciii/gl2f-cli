240d4bd9d8c4e2d2f1730e81bc372fd7ef942e33
trnciii <doityan223@gmail.com>
2022-07-17 10:33:02 +0900

    add gl2f.__init__

--- setup.py
+++ setup.py
@@ -5,12 +5,10 @@ setup(
 	version='0.0.1',
 	url='https://github.com/trnciii/gl2f-cli',
 	license='MIT',
-
-	packages=['gl2f'],
+	packages=find_packages(),
 	install_requires=[
 		'requests',
 	],
-
 	entry_points={
 		'console_scripts': [
 			'gl2blogs = gl2f.blogs:ls'
