6aaaed8375d455dedf17aa12734dde162c3c0011
trnciii <doityan223@gmail.com>
2022-08-04 23:06:56 +0900

    fix and change article style

--- gl2f/util/article.py
+++ gl2f/util/article.py
@@ -5,7 +5,7 @@ ptn_paragraph = re.compile(r'<p>(.*?)</p>')
 ptn_media = re.compile(r'<fns-media.*?type="(.+?)".*?></fns-media>')
 ptn_break = re.compile(r'<br>')
 ptn_link = re.compile(r'<a href="(.+?)".*?>.+?</a>')
-ptn_strong = re.compile(r'<strong>(.*?)</strong>')
+ptn_strong = re.compile(r'<strong.*?>(.*?)</strong>')
 ptn_span = re.compile(r'<span.*?>(.*?)</span>')
 ptn_http = re.compile(r'(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*))')
 
@@ -16,14 +16,14 @@ def paragraphs(body):
 
 def compose_line(p):
 	p = ptn_media.sub(term.mod(r'[\1]', [term.dim()]), p)
-	p = ptn_strong.sub(term.mod(r'\1', [term.color('white', 'fl'), term.color('black', 'bl')]), p)
-	p = ptn_link.sub(r'\1', p)
-	p = ptn_span.sub('', p)
+	p = ptn_strong.sub(term.mod(r'\1', [term.color('white', 'fl'), term.bold(), term.underline()]), p)
+	p = ptn_link.sub(r'\1 ', p)
+	p = ptn_span.sub(r'\1', p)
 	p = ptn_break.sub('', p)
 
 	# after processing tags
 	p = html.unescape(p)
-	p = ptn_http.sub(term.mod(r'\1 ', [term.color('cyan', 'fl')]), p)
+	p = ptn_http.sub(term.mod(r'\1', [term.color('blue', 'fl')]), p)
 
 	return p
 
