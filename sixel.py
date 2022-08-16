1a6f94ee04147ff59fa3335801c44d7e7b50986f
trnciii <doityan223@gmail.com>
2022-08-17 03:17:13 +0900

    update sixel prototype

--- sixel.py
+++ sixel.py
@@ -1,55 +1,58 @@
-from libsixel.encoder import Encoder
-from libsixel import SIXEL_OPTFLAG_WIDTH, SIXEL_OPTFLAG_HEIGHT
 import glob
-
-media_id = "688594503048627137"
-files = glob.glob(f"./{media_id}.*")
-assert len(files) == 1
-file = files[0]
-print(file)
-encoder = Encoder()
-encoder.encode(file)
-print("aaa")
-
-
-from libsixel import *
+import libsixel as sx
 from PIL import Image
 from io import BytesIO
 
-s = BytesIO()
 
-image = Image.open(file)
-width, height = image.size
-try:
-    data = image.tobytes()
-except NotImplementedError:
-    data = image.tostring()
-output = sixel_output_new(lambda data, s: s.write(data), s)
+def media_file_from_id(media_id):
+	files = glob.glob(f"./{media_id}.*")
+	assert len(files) == 1
+	return files[0]
 
-try:
-    if image.mode == 'RGBA':
-        dither = sixel_dither_new(256)
-        sixel_dither_initialize(dither, data, width, height, SIXEL_PIXELFORMAT_RGBA8888)
-    elif image.mode == 'RGB':
-        dither = sixel_dither_new(256)
-        sixel_dither_initialize(dither, data, width, height, SIXEL_PIXELFORMAT_RGB888)
-    elif image.mode == 'P':
-        palette = image.getpalette()
-        dither = sixel_dither_new(256)
-        sixel_dither_set_palette(dither, palette)
-        sixel_dither_set_pixelformat(dither, SIXEL_PIXELFORMAT_PAL8)
-    elif image.mode == 'L':
-        dither = sixel_dither_get(SIXEL_BUILTIN_G8)
-        sixel_dither_set_pixelformat(dither, SIXEL_PIXELFORMAT_G8)
-    elif image.mode == '1':
-        dither = sixel_dither_get(SIXEL_BUILTIN_G1)
-        sixel_dither_set_pixelformat(dither, SIXEL_PIXELFORMAT_G1)
-    else:
-        raise RuntimeError('unexpected image mode')
-    try:
-        # sixel_encode(data, width, height, 1, dither, output)
-        print(s.getvalue().decode('ascii'))
-    finally:
-        sixel_dither_unref(dither)
-finally:
-    sixel_output_unref(output)
+
+def img(file):
+	s = BytesIO()
+
+	image = Image.open(file)
+	width, height = image.size
+	try:
+		data = image.tobytes()
+	except NotImplementedError:
+		data = image.tostring()
+	output = sx.sixel_output_new(lambda data, s: s.write(data), s)
+
+	try:
+		if image.mode == 'RGBA':
+			dither = sx.sixel_dither_new(256)
+			sx.sixel_dither_initialize(dither, data, width, height, sx.SIXEL_PIXELFORMAT_RGBA8888)
+		elif image.mode == 'RGB':
+			dither = sx.sixel_dither_new(256)
+			sx.sixel_dither_initialize(dither, data, width, height, sx.SIXEL_PIXELFORMAT_RGB888)
+		elif image.mode == 'P':
+			palette = image.getpalette()
+			dither = sx.sixel_dither_new(256)
+			sx.sixel_dither_set_palette(dither, palette)
+			sx.sixel_dither_set_pixelformat(dither, sx.SIXEL_PIXELFORMAT_PAL8)
+		elif image.mode == 'L':
+			dither = sixel_dither_get(sx.SIXEL_BUILTIN_G8)
+			sx.sixel_dither_set_pixelformat(dither, sx.SIXEL_PIXELFORMAT_G8)
+		elif image.mode == '1':
+			dither = sixel_dither_get(sx.SIXEL_BUILTIN_G1)
+			sx.sixel_dither_set_pixelformat(dither, sx.SIXEL_PIXELFORMAT_G1)
+		else:
+			raise RuntimeError('unexpected image mode')
+
+		try:
+			sx.sixel_encode(data, width, height, 1, dither, output)
+			return s.getvalue().decode('ascii')
+		finally:
+			sx.sixel_dither_unref(dither)
+	finally:
+		sx.sixel_output_unref(output)
+
+
+if __name__ == '__main__':
+	media_id = "687482244532536160"
+	s = img(media_file_from_id(media_id)) or f'[image]({media_id})'
+	print(s)
+
