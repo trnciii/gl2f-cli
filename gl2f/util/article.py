4760a6107ecc848763adc35d4d0280531ed89bc4
trnciii <doityan223@gmail.com>
2022-08-16 11:06:02 +0900

    print media id in preview

--- gl2f/util/article.py
+++ gl2f/util/article.py
@@ -2,7 +2,7 @@ import re, html
 from gl2f.util import terminal as term
 
 ptn_paragraph = re.compile(r'<p>(.*?)</p>')
-ptn_media = re.compile(r'<fns-media.*?type="(.+?)".*?></fns-media>')
+ptn_media = re.compile(r'<fns-media.*?media-id="(.+?)".*?type="(.+?)".*?></fns-media>')
 ptn_break = re.compile(r'<br>')
 ptn_link = re.compile(r'<a href="(.+?)".*?>.+?</a>')
 ptn_strong = re.compile(r'<strong.*?>(.*?)</strong>')
@@ -15,8 +15,8 @@ def paragraphs(body):
 
 
 def compose_line(p):
-	p = ptn_media.sub(term.mod(r'[\1]', [term.dim()]), p)
-	p = ptn_strong.sub(term.mod(r'\1', [term.color('white', 'fl'), term.bold(), term.underline()]), p)
+	p = ptn_media.sub(term.mod('[\\2](\\1)', [term.dim()]), p)
+	p = ptn_strong.sub(term.mod('\\1', [term.color('white', 'fl'), term.bold(), term.underline()]), p)
 	p = ptn_link.sub(r'\1 ', p)
 	p = ptn_span.sub(r'\1', p)
 	p = ptn_break.sub('', p)
@@ -50,10 +50,7 @@ def extract_media(item):
 	import requests, urllib.request
 	from gl2f import auth
 
-	ptn_media_id = re.compile(r'<fns-media.*?media-id="(.+?)".*?></fns-media>')
-
-
-	for media_id in ptn_media_id.findall(item['values']['body']):
+	for media_id, _ in ptn_media.findall(item['values']['body']):
 
 		response = requests.get(
 			f"https://api.fensi.plus/v1/sites/girls2-fc/boards/{item['boardId']}/contents/{item['contentId']}/medias/{media_id}",
