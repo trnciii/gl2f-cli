355f8de6a263f05d85ba7d60a9c6f8874ca2a8cf
trnciii <doityan223@gmail.com>
2022-09-10 06:07:53 +0900

    decompose Lister class in functions

--- gl2f/core/lister.py
+++ gl2f/core/lister.py
@@ -7,61 +7,41 @@ from . import board, member
 from .date import is_today
 
 
-class Lister:
-	def __init__(self, name, debug=False):
-		self.name = name
-		self.debug = debug
+def fetch(domain, group, size, page, order='reservedAt:desc', categoryId=None, dump=False):
+	response = requests.get(
+		board.request_url(domain, group),
+		params={
+			'size': str(size),
+			'page': str(page),
+			'order': str(order),
+			'categoryId': categoryId
+		},
+		cookies={},
+		headers={
+			'origin': 'https://girls2-fc.jp',
+			'x-from': 'https://girls2-fc.jp',
+			'x-authorization': auth.update(auth.load()),
+		})
+
+	if not response.ok:
+		return
+
+	if dump:
+		import datetime
+		query = member.from_id(categoryId)[0] if categoryId else group
+		now = datetime.datetime.now().strftime('%y%m%d%H%M%S')
+		path = os.path.join(dump, f'{domain}-{query}-{now}.json')
+		with open(path, 'w') as f:
+			json.dump(response.json(), f, indent=2)
+
+	return response.json()
 
 
-	def fetch(self, group, size, page, order='reservedAt:desc', categoryId=None):
-		response = requests.get(
-			board.request_url(self.name, group),
-			params={
-				'size': str(size),
-				'page': str(page),
-				'order': str(order),
-				'categoryId': categoryId
-			},
-			cookies={},
-			headers={
-				'origin': 'https://girls2-fc.jp',
-				'x-from': 'https://girls2-fc.jp',
-				'x-authorization': auth.update(auth.load()),
-			})
-
-		if not response.ok:
-			return
-
-		if self.debug:
-			import datetime
-			query = member.from_id(categoryId)[0] if categoryId else group
-			now = datetime.datetime.now().strftime('%y%m%d%H%M%S')
-			path = os.path.join(self.debug, f'{self.name}-{query}-{now}.json')
-			with open(path, 'w') as f:
-				json.dump(response.json(), f, indent=2)
-
-		return response.json()
-
-
-	def list_group(self, group, size=10, page=1, order='reservedAt:desc'):
-		return self.fetch(group, size, page, order)['list']
-
-
-	def list_member(self, name, group=None, size=10, page=1, order='reservedAt:desc'):
-		member_data = member.get()[name]
-		categoryId = member_data['categoryId'][self.name]
-		group_list = member_data['group']
-		if not group in group_list:
-			group = group_list[0]
-
-		return self.fetch(group, size, page, order, categoryId=categoryId)['list']
-
-
-	def list_today(self):
-		return filter(
-			lambda i: is_today(i['openingAt']),
-			sum((self.fetch(group, size=10, page=1)['list'] for group in ['girls2', 'lucky2']), [])
-		)
+def list_member(domain, args):
+	data = member.get()[args.name]
+	group_list = data['group']
+	group = args.group if args.group in group_list else group_list[0]
+	return fetch(domain, group, args.number, args.page, args.order, categoryId=data['categoryId'][domain], dump=args.dump)['list']
 
 
 def add_args(parser):
@@ -85,35 +65,47 @@ def add_args(parser):
 
 
 def blogs(args):
-	lister = Lister('blog', debug=args.dump)
+	domain = 'blog'
 
 	if member.is_group(args.name):
-		return lister.list_group(args.name, args.number, args.page, order=args.order)
+		return fetch(domain, args.name, args.number, args.page, args.order, dump=args.dump)['list']
 
 	elif member.is_member(args.name):
-		return lister.list_member(args.name, group=args.group, page=args.page, size=args.number, order=args.order)
+		return list_member(domain, args)
 
 	elif args.name == 'today':
-		return lister.list_today()
+		return filter(
+			lambda i: is_today(i['openingAt']),
+			sum((fetch(domain, group, size=10, page=1, dump=args.dump)['list'] for group in ['girls2', 'lucky2']), [])
+		)
 
 
 def news(args):
-	lister = Lister('news', debug=args.dump)
-	return lister.list_group(args.name, args.number, args.page, args.order)
+	domain = 'news'
+
+	if args.name == 'today':
+		return filter(
+			lambda i: is_today(i['openingAt']),
+			fetch(domain, 'family', size=10, page=1, dump=args.dump)['list']
+		)
+
+	else:
+		return fetch(domain, args.name, args.number, args.page, args.order, dump=args.dump)['list']
 
 
 def radio(args):
-	lister = Lister('radio', debug=args.dump)
+	domain = 'radio'
 
 	if member.is_group(args.name):
-		return lister.list_group(args.name, args.number, args.page, args.order)
+		return fetch(domain, args.name, args.number, args.page, args.order, dump=args.dump)['list']
 
 	elif member.is_member(args.name):
-		return lister.list_member(args.name, args.group, args.number, args.page, order=args.order)
+		return list_member(domain, args)
+
 
 def pg(args):
-	lister = Lister('pg', debug=args.dump)
-	return lister.list_group(args.name, args.number, args.page, args.order)
+	domain = 'pg'
+	return fetch(domain, args.name, args.number, args.page, args.order, dump=args.dump)['list']
 
 
 def listers():
