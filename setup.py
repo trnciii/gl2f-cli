1656312c1221ee9a43099b0814ff8cb1632247ee
trnciii <doityan223@gmail.com>
2022-09-15 08:01:24 +0900

    memorial version hop

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='gl2f',
-	version='0.1.0',
+	version='0.1.1',
 	url='https://github.com/trnciii/gl2f-cli',
 	license='MIT',
 	packages=find_packages(),
