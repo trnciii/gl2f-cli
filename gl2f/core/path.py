39eced640f9818872e5557472bef33bfa6cedefc
trnciii <doityan223@gmail.com>
2022-09-16 21:11:10 +0900

    rename path functions

--- gl2f/core/path.py
+++ gl2f/core/path.py
@@ -1,6 +1,6 @@
 import os
 
-def return_path(path):
+def return_dir(path):
 	if not os.path.exists(path):
 		os.makedirs(path)
 	return path
@@ -8,12 +8,7 @@ def return_path(path):
 
 def home():
 	path = os.path.join(os.path.expanduser('~'), 'gl2f')
-	return return_path(path)
+	return return_dir(path)
 
-def ref(path):
-	return return_path(os.path.join(home(), path))
-
-
-if __name__ == '__main__':
-	print(home())
-	print(os.listdir(ref('media')))
+def refdir(path):
+	return return_dir(os.path.join(home(), path))
--- gl2f/dl.py
+++ gl2f/dl.py
@@ -13,7 +13,7 @@ def save(item, args):
 		out = os.path.join(args.o, contentId)
 		os.makedirs(out, exist_ok=True)
 	else:
-		out = path.ref(os.path.join('contents', contentId))
+		out = path.refdir(os.path.join('contents', contentId))
 
 	with open(os.path.join(out, f'{contentId}.json'), 'w') as f:
 		f.write(json.dumps(item, indent=2))
