1f727dc47021f36e919b7fc5dd97de636dd65cfa
trnciii <doityan223@gmail.com>
2023-01-28 15:32:47 +0900

    fix version check

--- check_version.py
+++ check_version.py
@@ -3,7 +3,7 @@ import sys, re
 with open('setup.py') as f:
 	texts = f.read()
 
-a = re.search(r'version=\'(.+)\',\n', texts).group(1)
+a = 'v' + re.search(r'version=\'(.+)\',\n', texts).group(1)
 b = sys.argv[1]
 print(a, b)
 assert a == b
