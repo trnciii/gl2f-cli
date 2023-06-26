3b84019ebd2061e595cb3e8b51c82cedab4fc4fa
trnciii <doityan223@gmail.com>
2023-06-26 19:35:45 +0900

    v0.3.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
 	name='gl2f',
 	description='CLI tool for GL2 family',
-	version='0.3.6',
+	version='0.3.7',
 	url='https://github.com/trnciii/gl2f-cli',
 	license='MIT',
 	packages=find_packages(),
