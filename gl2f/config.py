8cb4bb7cd697ab290713c49e23c29bd603562b9a
trnciii <doityan223@gmail.com>
2023-08-12 08:43:12 +0900

    add config edit command

--- gl2f/config.py
+++ gl2f/config.py
@@ -23,9 +23,28 @@ def path():
 	path = config.file()
 	print(path if os.path.isfile(path) else 'file does not exist')
 
+def edit():
+	from .ayame import terminal as term
+	from .core import config_editors
+
+	editors = config_editors.get()
+	data = config.load()
+
+	keys = list(editors.keys())
+	selected = term.select([f'{k}: {data[k]}' for k in keys])
+
+	for key in [k for k, s in zip(keys, selected) if s]:
+		try:
+			data[key] = editors[key]()
+		except Exception as e:
+			print(term.mod('error', term.color('red')), e)
+
+	config.save(data)
+
 def add_args(parser):
 	sub = parser.add_subparsers()
 
 	sub.add_parser('create').set_defaults(handler=lambda _:create())
 	sub.add_parser('path').set_defaults(handler=lambda _:path())
 	sub.add_parser('view').set_defaults(handler=lambda _:view())
+	sub.add_parser('edit').set_defaults(handler=lambda _:edit())
