77f35f538a81866064fdbffc3d249b4f633d629a
trnciii <doityan223@gmail.com>
2023-08-12 08:09:37 +0900

    check config file existence

--- gl2f/config.py
+++ gl2f/config.py
@@ -3,6 +3,11 @@ import os
 
 def view():
 	import json
+	path = config.file()
+	if not os.path.isfile(path):
+		print('file does not exist.')
+		return
+
 	with open(config.file(), encoding='utf-8') as f:
 		print(f.read())
 
@@ -14,9 +19,13 @@ def create():
 	config.save(config.default())
 	print(f'created "{config.file()}".')
 
+def path():
+	path = config.file()
+	print(path if os.path.isfile(path) else 'file does not exist')
+
 def add_args(parser):
 	sub = parser.add_subparsers()
 
 	sub.add_parser('create').set_defaults(handler=lambda _:create())
-	sub.add_parser('path').set_defaults(handler=lambda _:print(config.file()))
+	sub.add_parser('path').set_defaults(handler=lambda _:path())
 	sub.add_parser('view').set_defaults(handler=lambda _:view())
