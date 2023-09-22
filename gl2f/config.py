acb7604577c4c69364745df1a421c1e5e12a7033
trnciii <doityan223@gmail.com>
2023-09-23 02:04:59 +0900

    do not save a complete config

--- gl2f/config.py
+++ gl2f/config.py
@@ -3,24 +3,25 @@ import os
 
 def view():
 	import json
-	path = config.file()
+	path = config.filepath()
 	if not os.path.isfile(path):
 		print('file does not exist.')
 		return
 
-	with open(config.file(), encoding='utf-8') as f:
+	with open(path, encoding='utf-8') as f:
 		print(f.read())
 
 def create():
-	if os.path.isfile(config.file()):
+	path = config.filepath()
+	if os.path.isfile(path):
 		print('file already exits. abort.')
 		return
 
 	config.save(config.default())
-	print(f'created "{config.file()}".')
+	print(f'created "{path}".')
 
 def path():
-	path = config.file()
+	path = config.filepath()
 	print(path if os.path.isfile(path) else 'file does not exist')
 
 def edit():
--- gl2f/core/config.py
+++ gl2f/core/config.py
@@ -9,19 +9,20 @@ def default():
 		'serve-port': 7999,
 	}
 
-file = lambda: os.path.join(local.home(), 'config.json')
+def filepath():
+	return os.path.join(local.home(), 'config.json')
 
 def load():
 	import json
-	if os.path.isfile(file()):
-		with open(file(), encoding='utf-8') as f:
-			return {**default(), **json.loads(f.read())}
-	else:
-		return default()
+	path = filepath()
+	if os.path.isfile(path):
+		with open(path, encoding='utf-8') as f:
+			return json.loads(f.read())
+	return {}
 
 def save(data):
 	import json
-	with open(file(), 'w', encoding='utf-8') as f:
+	with open(filepath(), 'w', encoding='utf-8') as f:
 		f.write(json.dumps(data, indent=2, ensure_ascii=False))
 
-config = load()
+config = {**default(), **load()}
