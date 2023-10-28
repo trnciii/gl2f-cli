568bb9db0803419c5a9f3a22d3cbf2f93cc58231
trnciii <doityan223@gmail.com>
2023-10-29 04:37:59 +0900

    remove logger

--- gl2f/core/local.py
+++ gl2f/core/local.py
@@ -86,16 +86,3 @@ def package_data(f=''):
 	import gl2f
 	d, _ = os.path.split(gl2f.__file__)
 	return os.path.join(d, 'data', f)
-
-
-import subprocess
-repo = os.path.abspath(os.path.join(os.path.split(__file__)[0], '../../.git'))
-git = subprocess.run(f'git --git-dir {repo} log  -n1 --pretty=%h'.split(),
-	stdout=subprocess.PIPE, text=True, stderr=subprocess.DEVNULL, shell=(os.name == 'nt'))
-commit = git.stdout.rstrip('\n') if git.returncode == 0 else '-'*7
-
-def log(message):
-	from . import local
-	from datetime import datetime
-	with open(os.path.join(local.home(), 'log'), 'a') as f:
-		f.write(f'{datetime.now()} {commit} {message}\n')
