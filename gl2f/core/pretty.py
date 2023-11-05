95145f6198a22cc0497ae554a647f206ba2bc41d
trnciii <doityan223@gmail.com>
2023-11-06 00:48:37 +0900

    name regex groups

--- gl2f/core/pretty.py
+++ gl2f/core/pretty.py
@@ -2,7 +2,7 @@ from . import board, member, util
 from ..ayame import terminal as term, zen
 import re
 
-ptn_endspaces = re.compile(r'\s+(?P<newline>\n|$)')
+ptn_endspaces = re.compile(r'\s+(?P<end>\n|$)')
 
 class Formatter:
 	def __init__(self, f='author:title:url', fd=None, sep=' ', items=None):
@@ -105,7 +105,7 @@ class Formatter:
 
 
 	def format(self, item):
-		return ptn_endspaces.sub(r'\g<newline>',
+		return ptn_endspaces.sub(r'\g<end>',
 			self.sep.join(zen.ljust(self.functions[k](item), self.width.get(k, 0)) for k in self.keys())
 		)
 
