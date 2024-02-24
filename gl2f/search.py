97f136d6affa39f365739780ceebb7003312e871
trnciii <doityan223@gmail.com>
2024-02-24 14:51:58 +0900

    get search command structured

--- gl2f/search.py
+++ gl2f/search.py
@@ -1,60 +1,57 @@
-from .core import lister, pretty
+import re
+from .core import lister, pretty, article
 from .ayame import terminal as term
+
+class FilterResult:
+	def __init__(self, item, keywords):
+		self.item = item
+		self.text = ''.join(article.lines(item, 'plain', False))
+		self.score = sum(int(k in self.text) + int(k in item['values']['title']) for k in keywords)
 
 def merge(ranges):
 	if len(ranges) == 0: return []
 
 	ret = []
 	cur = ranges[0]
-	for j in range(1, len(ranges)):
-		if cur[1] < ranges[j][0]:
+	for r in ranges[1:]:
+		if cur[1] < r[0]:
 			ret.append(cur)
-			cur = ranges[j]
+			cur = r
 		else:
-			cur = cur[0], ranges[j][1]
+			cur = cur[0], r[1]
 	ret.append(cur)
 	return ret
 
+def print_highlighted(result, format, pattern, encoding, max_preview_lines=5):
+		term.write_with_encoding(pattern.sub(
+			term.mod(r'\g<match>', term.color('yellow'), term.inv()),
+			format(result.item)
+		) + '\n', encoding)
+
+		merged = merge([(i.start()-20, i.end()+20) for i in pattern.finditer(result.text)])
+		if len(merged) > max_preview_lines:
+			merged = merged[:max_preview_lines]
+
+		for begin, end in merged:
+			term.write_with_encoding('> ' + pattern.sub(
+				term.mod(r'\g<match>', term.color('yellow')),
+				result.text[max(0, begin):min(len(result.text), end)] + term.reset()
+			) + '\n', encoding)
+
+		term.write_with_encoding('\n', encoding)
 
 def subcommand(args):
-	from .core import article
-	import re
-
 	keywords = list(filter(len, sum((k.split('　') for k in args.keywords), [])))
 
+	items, _ = lister.list_contents(args)
+	results = filter(lambda i:i.score > 0, (FilterResult(i, keywords) for i in items))
+	if args.sort:
+		results = sorted(results, reverse=True, key=lambda x:x.score)
 
 	fm = pretty.from_args(args)
 	fm.reset_index(digits=len(str(args.number)))
-
-	hi = re.compile( fr"(?P<match>{'|'.join(keywords)})" )
-
-	items, _ = lister.list_contents(args)
-	texts = [''.join(article.lines(i, 'plain', False)) for i in items]
-	counts = [
-		sum(int(k in te) + int(k in ti) for k in keywords)
-		for te, ti in zip(texts, [i['values']['title'] for i in items])
-	]
-
-	for c, t, i in sorted(zip(counts, texts, items), reverse=True, key=lambda x:x[0]):
-		if c == 0: break
-
-		term.write_with_encoding(hi.sub(
-			term.mod(r'\g<match>', term.color('yellow'), term.inv()),
-			fm.format(i)
-		) + '\n', args.encoding)
-
-		ranges = [(i.start()-20, i.end()+20) for i in hi.finditer(t)]
-		merged = merge(ranges)
-		if len(merged)>5:
-			merged = merged[:5]
-
-		for begin, end in merged:
-			term.write_with_encoding('> ' + hi.sub(
-				term.mod(r'\g<match>', term.color('yellow')),
-				t[max(0, begin):min(len(t), end)] + term.reset()
-			) + '\n', args.encoding)
-
-		term.write_with_encoding('\n', args.encoding)
+	for i in results:
+		print_highlighted(i, fm.format, re.compile( fr"(?P<match>{'|'.join(keywords)})" ), args.encoding)
 
 
 def add_to():
@@ -70,6 +67,7 @@ def add_args(parser):
 
 	parser.add_argument('keywords', nargs='+')
 	parser.add_argument('--encoding')
+	parser.add_argument('--sort', action='store_true')
 
 	parser.set_defaults(handler=subcommand)
 
